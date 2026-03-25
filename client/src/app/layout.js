b8aab866343989726c4de5e4c16a98d95a7f2f3e
Developer <dev@example.com>
2026-03-25 12:00:00 +0530

    Create global Layout and Navbar components

---
+++ client/src/app/layout.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">Hobbies</div>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/feed">Feed</a>
+        <a href="/profile">Profile</a>
+      </div>
+    </nav>
+  );
+}
